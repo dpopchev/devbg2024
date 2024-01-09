352d30050fd9e059127ec04bd70f923d54f770c4
Dimitar Popchev <dpopchev@gmail.com>
2024-01-09 14:15:44 +0200

    add sulfuras

--- tests/gilded_rose/test_inventory_handling_sulfuras.py
+++ tests/gilded_rose/test_inventory_handling_sulfuras.py
@@ -2,18 +2,17 @@ import pytest
 from devbg2024.gilded_rose import Item, GildedRose
 from typing import NamedTuple
 
-ITEM_ID = 'General Item'
+ITEM_ID = 'Sulfuras'
 
 class SellInDecreaseRates(NamedTuple):
-    normal: int = 1
+    normal: int = 0
 
 class QualityDegradeRates(NamedTuple):
-    normal: int = 1
-    expired: int = 2
+    normal: int = 0
 
 class QualityLimits(NamedTuple):
-    min: int = 0
-    max: int = 50
+    min: int = 80
+    max: int = 80
 
 SELL_IN_DECREASE = SellInDecreaseRates()
 QUALITY_DEGRADE = QualityDegradeRates()
@@ -36,17 +35,3 @@ def test_quality_degrade_rate_within_sell_in(inventory: GildedRose):
     inventory.items.append(item)
     inventory.update_quality()
     assert item.quality == init_quality - QUALITY_DEGRADE.normal
-
-def test_quality_degrade_rate_after_sell_in(inventory: GildedRose):
-    init_quality = 10
-    item = Item(ITEM_ID, sell_in=0, quality=init_quality)
-    inventory.items.append(item)
-    inventory.update_quality()
-    assert item.quality == init_quality - QUALITY_DEGRADE.expired
-
-def test_quality_is_never_negative(inventory: GildedRose):
-    init_quality = 0
-    item = Item(ITEM_ID, sell_in=0, quality=init_quality)
-    inventory.items.append(item)
-    inventory.update_quality()
-    assert item.quality >= 0
