4fc85c562f45af06b293db1a617f6544bbeef306
Dimitar Popchev <dpopchev@gmail.com>
2024-01-09 14:36:03 +0200

    fix sulfurus item

--- src/devbg2024/gilded_rose.py
+++ src/devbg2024/gilded_rose.py
@@ -7,7 +7,7 @@ class GildedRose(object):
         for item in self.items:
             if item.name != "Aged Brie" and item.name != "Backstage passes":
                 if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
+                    if item.name != "Sulfuras":
                         item.quality = item.quality - 1
             else:
                 if item.quality < 50:
@@ -19,13 +19,13 @@ class GildedRose(object):
                         if item.sell_in < 6:
                             if item.quality < 50:
                                 item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
+            if item.name != "Sulfuras":
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
                 if item.name != "Aged Brie":
                     if item.name != "Backstage passes":
                         if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
+                            if item.name != "Sulfuras":
                                 item.quality = item.quality - 1
                     else:
                         item.quality = item.quality - item.quality
