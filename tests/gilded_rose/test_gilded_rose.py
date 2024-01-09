eba2a6c5e441a33bc1c314c7b0d4e38d919d45b4
Dimitar Popchev <dpopchev@gmail.com>
2024-01-09 14:37:38 +0200

    fix initial test

--- tests/gilded_rose/test_gilded_rose.py
+++ tests/gilded_rose/test_gilded_rose.py
@@ -4,12 +4,11 @@ from devbg2024.gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    @unittest.expectedFailure
     def test_foo(self):
         items = [Item("foo", 0, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEqual("fixme", items[0].name)
+        self.assertEqual("foo", items[0].name)
 
 
 if __name__ == '__main__':
