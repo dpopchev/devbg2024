a19dafedca2580cd6b53463882e1ffaa86b6a2f1
Dimitar Popchev <dpopchev@gmail.com>
2024-01-09 14:29:33 +0200

    test basckstage passes quality

--- tests/gilded_rose/test_inventory_handling_backstage_passes.py
+++ tests/gilded_rose/test_inventory_handling_backstage_passes.py
@@ -2,14 +2,15 @@ import pytest
 from devbg2024.gilded_rose import Item, GildedRose
 from typing import NamedTuple
 
-ITEM_ID = 'General Item'
+ITEM_ID = 'Backstage passes'
 
 class SellInDecreaseRates(NamedTuple):
     normal: int = 1
 
 class QualityDegradeRates(NamedTuple):
     normal: int = 1
-    expired: int = 2
+    ten_days_mark: int = -2
+    five_days_mark: int = -3
 
 class QualityLimits(NamedTuple):
     min: int = 0
@@ -23,30 +24,30 @@ QUALITY_LIMITS = QualityLimits()
 def inventory():
     return GildedRose([])
 
-def test_sell_in_decrease_rate(inventory: GildedRose):
-    init_sell_in = 10
-    item = Item(ITEM_ID, sell_in=init_sell_in, quality=10)
-    inventory.items.append(item)
-    inventory.update_quality()
-    assert item.sell_in == init_sell_in - SELL_IN_DECREASE.normal
-
-def test_quality_degrade_rate_within_sell_in(inventory: GildedRose):
+def test_quality_degrade_rate_before_10_day_sell_in_mark(inventory: GildedRose):
     init_quality = 10
-    item = Item(ITEM_ID, sell_in=10, quality=init_quality)
+    item = Item(ITEM_ID, sell_in=11, quality=init_quality)
     inventory.items.append(item)
     inventory.update_quality()
     assert item.quality == init_quality - QUALITY_DEGRADE.normal
 
-def test_quality_degrade_rate_after_sell_in(inventory: GildedRose):
+def test_quality_degrade_rate_in_10_to_5_days_sell_in_range(inventory: GildedRose):
+    init_quality = 10
+    item = Item(ITEM_ID, sell_in=8, quality=init_quality)
+    inventory.items.append(item)
+    inventory.update_quality()
+    assert item.quality == init_quality - QUALITY_DEGRADE.ten_days_mark
+
+def test_quality_degrade_rate_after_5_days_sell_in_mark(inventory: GildedRose):
+    init_quality = 10
+    item = Item(ITEM_ID, sell_in=4, quality=init_quality)
+    inventory.items.append(item)
+    inventory.update_quality()
+    assert item.quality == init_quality - QUALITY_DEGRADE.five_days_mark
+
+def test_quality_is_min_after_the_concert(inventory: GildedRose):
     init_quality = 10
     item = Item(ITEM_ID, sell_in=0, quality=init_quality)
     inventory.items.append(item)
     inventory.update_quality()
-    assert item.quality == init_quality - QUALITY_DEGRADE.expired
-
-def test_quality_is_never_negative(inventory: GildedRose):
-    init_quality = 0
-    item = Item(ITEM_ID, sell_in=0, quality=init_quality)
-    inventory.items.append(item)
-    inventory.update_quality()
-    assert item.quality >= 0
+    assert item.quality == QUALITY_LIMITS.min
