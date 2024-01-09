9806c54cecf86fdf5bec81b3255b0cbf4732e448
Dimitar Popchev <dpopchev@gmail.com>
2024-01-09 13:10:45 +0200

    init as copy

---
+++ tests/gilded_rose/test_sell_in_decrease_rate.py
@@ -0,0 +1,34 @@
+import pytest
+from devbg2024.gilded_rose import Item, GildedRose
+from typing import NamedTuple
+
+class DegradeRates(NamedTuple):
+    normal: int
+    expired: int
+
+DEGRADE_RATES = DegradeRates(1, 2)
+
+@pytest.fixture
+def inventory():
+    return GildedRose([])
+
+def test_degrade_rate_before_sell_in_has_passed_is_normal(inventory: GildedRose):
+    initial_quality = 5
+    item = Item('item', sell_in=1, quality=initial_quality)
+    inventory.items.append(item)
+    inventory.update_quality()
+    assert item.quality == initial_quality - DEGRADE_RATES.normal
+
+def test_degrade_rate_after_sell_in_has_passed_is_expired(inventory: GildedRose):
+    initial_quality = 5
+    item = Item('item', sell_in=0, quality=initial_quality)
+    inventory.items.append(item)
+    inventory.update_quality()
+    assert item.quality == initial_quality - DEGRADE_RATES.expired
+
+def test_degrade_rate_of_aged_brie_is_negative(inventory: GildedRose):
+    initial_quality = 5
+    item = Item('Aged Brie', sell_in=1, quality=initial_quality)
+    inventory.items.append(item)
+    inventory.update_quality()
+    assert item.quality > initial_quality
