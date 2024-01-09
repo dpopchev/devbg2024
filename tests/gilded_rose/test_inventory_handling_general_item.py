430832d91b12ed4be2d9fdd7074147d359482b24
Dimitar Popchev <dpopchev@gmail.com>
2024-01-09 14:00:09 +0200

    refactor by making id module constnat

--- tests/gilded_rose/test_inventory_handling_general_item.py
+++ tests/gilded_rose/test_inventory_handling_general_item.py
@@ -2,6 +2,8 @@ import pytest
 from devbg2024.gilded_rose import Item, GildedRose
 from typing import NamedTuple
 
+ITEM_ID = 'General Item'
+
 class SellInDecreaseRates(NamedTuple):
     normal: int = 1
 
@@ -23,28 +25,28 @@ def inventory():
 
 def test_sell_in_decrease_rate(inventory: GildedRose):
     init_sell_in = 10
-    item = Item('item', sell_in=init_sell_in, quality=10)
+    item = Item(ITEM_ID, sell_in=init_sell_in, quality=10)
     inventory.items.append(item)
     inventory.update_quality()
     assert item.sell_in == init_sell_in - SELL_IN_DECREASE.normal
 
 def test_quality_degrade_rate_within_sell_in(inventory: GildedRose):
     init_quality = 10
-    item = Item('item', sell_in=10, quality=init_quality)
+    item = Item(ITEM_ID, sell_in=10, quality=init_quality)
     inventory.items.append(item)
     inventory.update_quality()
     assert item.quality == init_quality - QUALITY_DEGRADE.normal
 
 def test_quality_degrade_rate_after_sell_in(inventory: GildedRose):
     init_quality = 10
-    item = Item('item', sell_in=0, quality=init_quality)
+    item = Item(ITEM_ID, sell_in=0, quality=init_quality)
     inventory.items.append(item)
     inventory.update_quality()
     assert item.quality == init_quality - QUALITY_DEGRADE.expired
 
 def test_quality_is_never_negative(inventory: GildedRose):
     init_quality = 0
-    item = Item('item', sell_in=0, quality=init_quality)
+    item = Item(ITEM_ID, sell_in=0, quality=init_quality)
     inventory.items.append(item)
     inventory.update_quality()
     assert item.quality >= 0
